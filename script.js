e0fcb711771c73991a2df90ffd0a9acac2fdea5c
Moses Mwai <148555858+mottale11@users.noreply.github.com>
2024-11-07 23:03:43 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+let greeting = "Hello" + " " + "World!";
+let name = "Soila";
+let welcomemessage = "WElcome, " + name;
+
+// 3. Logical Expressions
+let x = 10;
+let y = 5;
+
+let isgreater = x > y && y < x;
+let isEither = x < y || y < x;
+let isNotTrue = !false;
+
+// 4. Comparison Expressions
+let a = 5
+let b = "5"
+
+let isEqual = a == b;
+let IsStrictEqual = a === b;
+let isGreater = a > 3;
+
+
+// 9. Expressions with typeof and instanceof
+let num = 42;
+console.log(typeof num);         // "number"
+console.log(person instanceof Object); // true if `person` is an object
+
+// 10. Expressions with new (Creating Objects)
+let date = new Date(); // Creates a new Date object
+let obj = new Object(); // Creates a new Object
